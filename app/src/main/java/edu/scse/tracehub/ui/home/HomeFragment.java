29488a1c9719c794270baba3f4662f154390b21e
anleeos <2937160075@qq.com>
2021-05-09 12:11:55 +0800

    定位

--- app/src/main/java/edu/scse/tracehub/ui/home/HomeFragment.java
+++ app/src/main/java/edu/scse/tracehub/ui/home/HomeFragment.java
@@ -43,13 +43,13 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
     public static final LatLng TIANJIN = new LatLng(39.13,117.2);// 杭州市经纬度
     protected static CameraPosition cameraPosition;
 
-    //定位相关
     //声明AMapLocationClient类对象，定位发起端
     private AMapLocationClient mLocationClient = null;
     //声明mLocationOption对象，定位参数
     public AMapLocationClientOption mLocationOption = null;
     //声明mListener对象，定位监听器
     private OnLocationChangedListener mListener = null;
+    //标识，用于判断是否只显示一次定位信息和用户重新定位
     private boolean isFirstLoc = true;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -88,48 +88,17 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
                 aMap.moveCamera(CameraUpdateFactory.newCameraPosition(getCameraPosition()));
             }
         }
+        UiSettings settings = aMap.getUiSettings();
+        aMap.setLocationSource(this);//设置了定位的监听
+        // 是否显示定位按钮
+        settings.setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);//显示定位层并且可以触发定位,默认是flase
+        //开始定位
+        location();
     }
-    @Override
-    public void onResume() {
-        super.onResume();
-        textureMapView.onResume();
-    }
-    /**
-     * 方法必须重写
-     */
-    @Override
-    public void onPause() {
-        super.onPause();
-        textureMapView.onPause();
-    }
-
-    /**
-     * 方法必须重写
-     */
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        textureMapView.onSaveInstanceState(outState);
-    }
-
-    /**
-     * 方法必须重写
-     */
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        setCameraPosition(aMap.getCameraPosition());
-        super.onDestroy();
-        textureMapView.onDestroy();
-        mLocationClient.stopLocation();//停止定位
-        mLocationClient.onDestroy();//销毁定位客户端。
-    }
-
-    // 实现定位的函数
-    public void location()
-    {
+    private void location() {
         //初始化定位
-        mLocationClient = new AMapLocationClient(this.getActivity().getApplicationContext());
+        mLocationClient = new AMapLocationClient(this.getContext());
         //设置定位回调监听
         mLocationClient.setLocationListener(this);
         //初始化定位参数
@@ -141,7 +110,7 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
         //设置是否只定位一次,默认为false
         mLocationOption.setOnceLocation(false);
         //设置是否强制刷新WIFI，默认为强制刷新
-        mLocationOption.setWifiActiveScan(true);
+        //mLocationOption.setWifiActiveScan(true);
         //设置是否允许模拟位置,默认为false，不允许模拟位置
         mLocationOption.setMockEnable(false);
         //设置定位间隔,单位毫秒,默认为2000ms
@@ -151,7 +120,6 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
         //启动定位
         mLocationClient.startLocation();
     }
-
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
         if (aMapLocation != null) {
@@ -193,7 +161,7 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
                             + aMapLocation.getDistrict() + ""
                             + aMapLocation.getStreet() + ""
                             + aMapLocation.getStreetNum());
-                    Toast.makeText(this.getActivity().getApplicationContext(), buffer.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(this.getContext(), buffer.toString(), Toast.LENGTH_LONG).show();
                     isFirstLoc = false;
                 }
 
@@ -203,7 +171,7 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
                 Log.e("AmapError", "location Error, ErrCode:"
                         + aMapLocation.getErrorCode() + ", errInfo:"
                         + aMapLocation.getErrorInfo());
-                Toast.makeText(this.getActivity().getApplicationContext(), "定位失败", Toast.LENGTH_LONG).show();
+                Toast.makeText(this.getContext(), "定位失败", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -215,5 +183,40 @@ public class HomeFragment extends Fragment implements AMapLocationListener, Loca
     @Override
     public void deactivate() {
         mListener = null;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        textureMapView.onResume();
+    }
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        textureMapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        textureMapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        setCameraPosition(aMap.getCameraPosition());
+        super.onDestroy();
+        textureMapView.onDestroy();
+        mLocationClient.stopLocation();//停止定位
+        mLocationClient.onDestroy();//销毁定位客户端。
     }
 }
