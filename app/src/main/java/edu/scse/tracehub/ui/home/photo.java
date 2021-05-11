1a089643c246378b7e33b310e972e23bc510baa0
shenjiawei <1066279068@qq.com>
2021-05-11 21:32:50 +0800

    拍照

--- app/src/main/java/edu/scse/tracehub/ui/home/photo.java
+++ app/src/main/java/edu/scse/tracehub/ui/home/photo.java
@@ -1,6 +1,8 @@
 package edu.scse.tracehub.ui.home;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,7 @@ import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 public class photo extends Fragment {
-    private Button mBtnInputFragment3, mBtnBack;
+    private Button mBtnBack,mphoto;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,12 +31,24 @@ public class photo extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.shangchuan, container, false);
         mBtnBack = view.findViewById(R.id.back2);
+        mphoto = view.findViewById(R.id.photo);
         mBtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(getView()).popBackStack(); //返回上一个碎片
             }
         });
+        mphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
         return view;
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
     }
 }
